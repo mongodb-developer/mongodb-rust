673abf9bd3397231fa6b5fb2f73bacd1c9f7aef7
Joel Lord <joelphy@gmail.com>
2021-11-02 08:57:50 -0400

    Update main.rs

--- app/rust_quickstart/src/main.rs
+++ app/rust_quickstart/src/main.rs
@@ -5,7 +5,7 @@ use tokio;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
    // Create a client that connects to the computer_scientists collection
-   let options = ClientOptions::parse("mongodb+srv://blog:blog@cluster0.2grje.mongodb.net/test?authSource=admin&replicaSet=atlas-v6xmes-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true").await?;
+   let options = ClientOptions::parse("CONN_STRING").await?;
    let client = mongodb::Client::with_options(options)?;
    let theaters: Collection<Document> = client.database("sample_mflix").collection("theaters");
    // Look up one document:
