97984fadbd323eb6971ec4e671c42b523402ba0e
Joel Lord <joelphy@gmail.com>
2021-11-03 16:28:11 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,10 @@
 # Start a container with the Rust runtimes (1.56)
 docker run -it -v $(pwd)/app:/opt/app:z rust:1.56 /bin/bash
 
-# Run the following commands in the container
+# Create the rust application template (only once)
+cd /opt/app
+cargo new --bin rust_quickstart
+
+# Run the following commands in the container to compile and run the application
 cd /opt/app/rust_quickstart
 cargo run
