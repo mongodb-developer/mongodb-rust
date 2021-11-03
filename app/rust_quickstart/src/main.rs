b3295df11e7dbfd2b7488fb7d4f90171bfc3a343
Joel Lord <joelphy@gmail.com>
2021-11-03 16:38:59 -0400

    Update main.rs

--- app/rust_quickstart/src/main.rs
+++ app/rust_quickstart/src/main.rs
@@ -1,16 +1,19 @@
-use mongodb::{options::ClientOptions, Collection, bson::Document};
+use mongodb::{options::ClientOptions, Collection, bson::Document, bson::doc};
 use std::error::Error;
 use tokio;
-
+ 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-   // Create a client that connects to the computer_scientists collection
-   let options = ClientOptions::parse("CONN_STRING").await?;
-   let client = mongodb::Client::with_options(options)?;
-   let theaters: Collection<Document> = client.database("sample_mflix").collection("theaters");
-   // Look up one document:
-   let theater = theaters.find_one(None, None,).await?.expect("No matching documents found.");
-   println!("Theater: {}", theater);
-
-   Ok(())
+  // Create a client that connects to the computer_scientists collection
+  let options = ClientOptions::parse("<CONN_STRING>").await?;
+  let client = mongodb::Client::with_options(options)?;
+  let theaters: Collection<Document> = client.database("sample_mflix").collection("theaters");
+  // Look up one document:
+  let theater = theaters
+                  .find_one(doc! {"theaterId": 1043}, None,)
+                  .await
+                  ?.expect("No matching documents found.");
+  println!("Theater: {}", theater);
+ 
+  Ok(())
 }
